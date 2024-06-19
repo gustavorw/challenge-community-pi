6dc4c434210085678fb06c2ce90bb89c73262288
gustavorw <gustavorw03@gmail.com>
2024-06-19 17:28:41 -0300

    #0 desafio 01

---
+++ challenge_0/lib/assets/images/app_images.dart
@@ -0,0 +1,6 @@
+class AppImages {
+  AppImages._();
+  static String image01 = 'assets/images/01.jpg';
+  static String image02 = 'assets/images/02.jpg';
+  static String image03 = 'assets/images/03.jpg';
+}
---
+++ challenge_0/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:challenge/assets/images/app_images.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Scaffold(),
+    );
+  }
+}
